95ece19ed7d422a71dc551507ac24a779df63bc4
ms3c <63225928+ms3c@users.noreply.github.com>
2020-04-06 00:11:41 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Python is easy
+"""
+
+def Song(SongName = "Tears always win"):
+  Output = SongName
+  return Output
+  
+print(Song())
+  
+def SongArtist(Artist = "Alicia Keys"):
+  Output = Artist
+  return Output
+  
+print(SongArtist())
